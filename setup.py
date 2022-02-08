fdfc4993678a639f8d04055cf649fde7ddcb9386
SalimTerryLi <lhf2613@gmail.com>
2022-02-08 14:49:47 +0800

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='pyAsyncBot',
     packages=find_packages(include=['pyasyncbot', 'pyasyncbot.*']),
-    version='0.1.2',
+    version='0.1.3',
     description='An async chat bot client framework',
     author='SalimTerryLi',
     license='MIT',
